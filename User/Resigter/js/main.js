5f21dcf3333f89c77c4442c279afcefbd8ce4cbb
Omar <chida1u@fst-ats-319-l03.fst.site.univ-lorraine.fr>
2019-11-26 12:18:37 +0100

    Added signup formula.

---
+++ User/Resigter/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+    // USE STRICT
+    "use strict";
+
+    $(".form-radio .radio-item").click(function(){
+        //Spot switcher:
+        $(this).parent().find(".radio-item").removeClass("active");
+        $(this).addClass("active");
+    });
+
+})(jQuery);
