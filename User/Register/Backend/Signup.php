a4b6040d066592667ae1c991fec48926df338dd2
Omar <omar@asus>
2019-12-08 17:54:51 +0100

    Update Signup.php, users.db, and 3 more files...

--- User/Register/Backend/Signup.php
+++ User/Register/Backend/Signup.php
@@ -14,6 +14,25 @@
         phone_number VARCHAR(16)
     )");
 
-    echo ".$_POST[user].",".$_POST[password].",".$_POST[name].",".$_POST[last_name].",".$_POST[sexe].",".$_POST[email].",".$_POST[bdate].",".$_POST[adress].",".$_POST[phone].";
-    $db->exec("INSERT INTO users(username, password_h, name, last_name, sexe, email, birthdate, adress, phone_number) VALUES('".$_POST['user']."','".$_POST['password']."','".$_POST['name']."','".$_POST['last_name']."','".$_POST['sexe']."','".$_POST['email']."','".$_POST['bdate']."','".$_POST['adress']."','".$_POST['phone']."')");
+    $info = json_decode(json_encode($_POST), true);
+    $res = $db->query("SELECT * FROM users WHERE username = '$info[user]'");
+
+    if ( (!isset($info[user]) || !isset($info[password])) || ($info[user] == "" || $info[password] == "") ){
+        echo "<a href='#user' class='vertify-booking'>Please enter username and password.</a>";
+        return;
+    }
+
+    if (isset($info[email]) && $info[email] != "" && !filter_var($info[email], FILTER_VALIDATE_EMAIL)) {
+        echo "<a href='#email' class='vertify-booking'>The email : '$info[email]' is not a valid mail adress.</a>";
+        return;
+    }
+
+    if ($row = $res->fetchArray()){
+        echo "<a href='#user' class='vertify-booking'>The username : '$info[user]' already exists please choose another username.</a>";
+        return;
+    }
+
+   
+    $pwd_hash = hash("sha256", $info[password]);
+    $db->exec("INSERT INTO users(username, password_h, name, last_name, sexe, email, birthdate, adress, phone_number) VALUES('".$info[user]."','".$pwd_hash."','".$info[name]."','".$info[last_name]."','".$info[sexe]."','".$info[email]."','".$info[bdate]."','".$info[adress]."','".$info[phone]."')");
 ?>
